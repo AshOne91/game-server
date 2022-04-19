7290ed8cec23c0b75a7615a6de03d76ac1de4c42
AshOne91 <ldgo91@gmail.com>
2022-04-19 13:35:16 +0900

    기본 프로젝트 커밋

---
+++ game-server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace game_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
