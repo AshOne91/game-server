74ff074b6ec7f454528c2547934e1190feb19cad
DESKTOP-F99BNKH\kgca40647 <ldgo91@gmail.com>
2022-07-04 19:00:31 +0900

    작업중 커밋

---
+++ Template/Item/GameBaseItem/Common/GameBaseItemDBTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Service.Net;
+using Service.Core;
+using Service.DB;
+
+namespace GameBase.Template.Item.GameBaseItem.Common
+{
+    public class 
+    {
+    }
+}
