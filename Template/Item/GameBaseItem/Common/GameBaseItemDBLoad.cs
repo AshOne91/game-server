704b803bd278943b30e014e4391af8c41e2c2840
DESKTOP-F99BNKH\kgca40647 <ldgo91@gmail.com>
2022-07-05 18:58:58 +0900

    작업중

---
+++ Template/Item/GameBaseItem/Common/GameBaseItemDBLoad.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Service.Net;
+using Service.Core;
+using Service.DB;
+
+namespace GameBase.Template.Item.GameBaseItem.Common
+{
+    public class GameBaseItemDBLoad : ItemDBLoad
+    {
+        private void _Run_LoadUser_DBItem(AdoDB adoDB, UInt64 user_db_key, UInt64 player_db_key, DBSlotContainer_DBItemTable container)
+        {
+            try
+            {
+                QueryBuilder query = new QueryBuilder("call gp_player_dbitemtable_load");
+
+                query.SetInputParam("@p_user_db_key", user_db_key);
+                query.SetInputParam("@p_player_db_key", player_db_key);
+
+                adoDB.Execute(query);
+
+                while (adoDB.RecordWhileNotEOF())
+                {
+                    short nSlot = adoDB.RecordGetValue("slot");
+
+                    DBSlot_DBItemTable slot = container.Insert(nSlot, false);
+
+                    slot._isDeleted = adoDB.RecordGetValue("deleted");
+                    slot._DBData.user_db_key = adoDB.RecordGetValue("")
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("[gp_player_dbitemtable_load]" + e.Message);
+            }
+        }
+    }
+}
---
+++ Template/Item/GameBaseItem/Common/GameBaseItemUserDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Service.Net;
+using Service.Core;
+using Service.DB;
+using GameBase.Template.GameBase;
+
+namespace GameBase.Template.Item.GameBaseItem.Common
+{
+    public class GameBaseItemUserDB : ItemUserDB
+    {
+        public DBSlotContainer_DBItemTable _dbSlotContainer_DBItemTable = new DBSlotContainer_DBItemTable();
+
+        void override ItemCopy(UserDB userSrc, bool isChanged)
+        {
+            GameBaseItemUserDB _ItemUserDB = userSrc._ItemUserDB;
+            _dbSlotContainer_DBItemTable.Copy(_ItemUserDB._dbSlotContainer_DBItemTable, isChanged);
+        }
+    }
+}
